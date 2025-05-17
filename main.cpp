d2998d46ff3a3b027ded1637b9c8b8ce24736708
leandro bandeira <leoviolinist22@gmail.com>
2025-05-17 20:47:00 -0300

    adding new file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+//use filyesistem
+using namespace std;
+
+
+
+
+int main(){
+  string command = "";
+
+  while(command != "exit"){
+    if(command == "copy ."){
+      
+    }
+  }  
+  return 0;
+}
